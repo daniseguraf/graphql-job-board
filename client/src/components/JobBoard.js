ee4059e72dc373b06c761253ce37f46d733ae415
Daniel Segura <danielseguraf@gmail.com>
2023-01-23 23:59:42 -0500

    resolvers

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,12 +1,22 @@
 import JobList from './JobList';
-import { jobs } from '../fake-data';
+import { getJobs } from '../queries/queries';
+import { useEffect, useState } from 'react';
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+
+  const fetch = async () => {
+    const response = await getJobs();
+    setJobs(response);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
