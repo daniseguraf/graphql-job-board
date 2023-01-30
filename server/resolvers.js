afb0eca00a791859139034973f5548686254051f
Daniel Segura <danielseguraf@gmail.com>
2023-01-29 23:59:15 -0500

    company query

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,8 +4,10 @@ export const resolvers = {
   Query: {
     jobs: () => Job.findAll(),
     job: (_root, { id }) => Job.findById(id),
+    company: (id) => Company.findById(id),
   },
   Job: {
     company: (job) => Company.findById(job.companyId),
   },
+  // Company: {},
 };
